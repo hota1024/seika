e9a7daee09594761f4996bee8ed38353a9b7db23
HotakaOkumoto <okumoto@ptyhard.co.jp>
2025-06-26 11:12:20 +0900

    format: improve indentation in Result type definition

--- src/result-type.ts
+++ src/result-type.ts
@@ -1,9 +1,9 @@
 export type Result<T, E> =
-	| {
-			kind: "ok";
-			inner: T;
-	  }
-	| {
-			kind: "err";
-			inner: E;
-	  };
+  | {
+      kind: "ok";
+      inner: T;
+    }
+  | {
+      kind: "err";
+      inner: E;
+    };
